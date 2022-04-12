ef959d0b77bdcde9989a0d7f564c8ddc0ec7de6d
Paolo Donadeo <paolo@donadeo.net>
2022-04-12 19:59:50 +0200

    Grafica completamente rifatta da Valeria La Mantia. Closes #2

--- src/views.eml.ml
+++ src/views.eml.ml
@@ -21,60 +21,111 @@ let risposta_no giorni_fa data_italiano _ep_num _titolo =
 
 let index uscito fretta giorni_fa data_italiano ep_num titolo =
   <!DOCTYPE html>
-  <html lang="en">
+  <html lang="it">
 
   <head>
-      <meta charset="UTF-8">
-      <meta name="google-site-verification" content="RVQl_YUzLnpzfD8LfW0RND3EgYD_grzfjKCwshqJ1Xg" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>È uscito Joypad?</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="google-site-verification" content="RVQl_YUzLnpzfD8LfW0RND3EgYD_grzfjKCwshqJ1Xg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>È uscito Joypad?</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+      rel="stylesheet">
 
-      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png">
-      <link rel="manifest" href="/static/site.webmanifest">
-      <style>
-          body {
-              font-family: 'Montserrat', sans-serif;
-              text-align: center;
-              background-color: beige;
-              line-height: 1.4;
-          }
 
-          h1 {
-              margin: auto;
-              margin-top: 1em;
-              width: 75%;
-          }
+    <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png">
+    <link rel="manifest" href="/static/site.webmanifest">
+    <style>
+      body {
+        min-height: calc(100vh - 20px);
+        margin: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        font-family: 'Montserrat', sans-serif;
+        text-align: left;
+        background-color: #f7f7f7;
+      }
 
-          p {
-            margin: auto;
-            margin-top: 1em;
-            text-align: left;
-            width: 75%;
-            font-style: italic;
-            font-size: 20px;
-          }
+      header {
+        display: flex;
+        justify-content: center;
+        height: 30vh;
+      }
 
-          p.nota {
-            margin-top: 4em;
-            font-style: italic;
-            font-size: 12px;
-          }
+      header img {
+        height: 100%;
+      }
 
-          footer {
-            margin-top: 10em;
-            font-size: 10px;
-            line-height: 1.4;
-          }
-      </style>
+      h1 {
+        margin: auto;
+        width: 80%;
+        font-size: 20px;
+        line-height: 25px;
+        font-weight: 700;
+        margin-top: 10px;
+        margin-bottom: 10px;
+      }
+
+      h1 em {
+        font-weight: 900;
+      }
+
+      p {
+        margin: auto;
+        text-align: left;
+        width: 80%;
+        font-size: 15px;
+        line-height: 20px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+      }
+
+      .nota {
+        font-style: italic;
+        font-size: 12px;
+        line-height: 15px;
+      }
+
+      a {
+        color: #29ABE2;
+        font-weight: 700;
+        text-decoration: unset;
+        transition: color 0.2s;
+        -webkit-transition: color 0.2s;
+      }
+
+      a:hover {
+        color: #313131;
+        transition: color 0.2s;
+        -webkit-transition: color 0.2s;
+      }
+
+      footer {
+        height: 30px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+      }
+
+      footer p {
+        font-size: 10px;
+        line-height: 12px;
+        text-align: center;
+      }
+    </style>
   </head>
 
   <body>
+    <header>
+      <img src="/static/joypad-img.jpg" alt="È uscito Joypad?">
+    </header>
+
+    <main>
       <%s! if uscito
         then risposta_si giorni_fa data_italiano ep_num titolo
         else risposta_no giorni_fa data_italiano ep_num titolo %>
@@ -88,12 +139,12 @@ let index uscito fretta giorni_fa data_italiano ep_num titolo =
         potete fare tutte le richieste che volete
         <a href="https://github.com/pdonadeo/e-uscito-joypad/issues">qui</a>.
       </p>
-  </body>
+    </main>
 
-  <footer>
-    Copyright © 2022 Paolo Donadeo.
-    <br>
-    Rilasciato sotto licenza MIT, vedi <a href="https://github.com/pdonadeo/e-uscito-joypad/blob/main/LICENSE">LICENSE.md</a>
-  </footer>
+    <footer>
+      <p>Copyright © 2022 Paolo Donadeo.<br>Rilasciato sotto licenza MIT, vedi <a
+          href="https://github.com/pdonadeo/e-uscito-joypad/blob/main/LICENSE">LICENSE.md</a></p>
+    </footer>
+  </body>
 
   </html>
