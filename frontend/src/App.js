fa3aa79f286c6685a1c9054140c4c3187d6ce9dd
Paolo Donadeo <paolo@donadeo.net>
2022-04-25 14:54:07 +0200

    React: fix spazi bianchi adiacenti a tag HTML

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,11 +20,11 @@ function App() {
     {datiUltimaPuntata.uscito ? <RispostaSi dati={datiUltimaPuntata} /> : <RispostaNo dati={datiUltimaPuntata} />}
 
     <p>
-      Ascolta la puntata <a href="https://www.ilpost.it/podcasts/joypad/">sulla pagina del Post</a>,&nbsp;
-      sull’<a href="https://app.ilpost.it/">app</a> o sulle principali piattaforme:&nbsp;
-      <a href="https://open.spotify.com/show/2JXX6er1IfNvbyPbYPI9tJ">Spotify</a>,&nbsp;
-      <a href="https://podcasts.apple.com/us/podcast/joypad/id1491137742?uo=4">Apple Podcast</a> e&nbsp;
-      <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1MTQyOS9lcGlzb2Rlcy9mZWVk">Google Podcast</a>.
+      Ascolta la puntata <a href="https://www.ilpost.it/podcasts/joypad/">sulla pagina del Post</a>,
+      sull’<a href="https://app.ilpost.it/">app</a> o sulle principali piattaforme: <a
+      href="https://open.spotify.com/show/2JXX6er1IfNvbyPbYPI9tJ">Spotify</a>, <a
+      href="https://podcasts.apple.com/us/podcast/joypad/id1491137742?uo=4">Apple Podcast</a> e <a
+      href="https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1MTQyOS9lcGlzb2Rlcy9mZWVk">Google Podcast</a>.
     </p>
 
     {datiUltimaPuntata.fretta ? <p>
@@ -32,10 +32,10 @@ function App() {
     </p> : ""
     }
 
-    <p class="nota">Nota per gli autori del podcast: il sito è pubblicato gratuitamente e con licenza open su&nbsp;
-      <a href="https://github.com/pdonadeo/e-uscito-joypad">GitHub</a> e&nbsp;
-      potete fare tutte le richieste che volete&nbsp;
-      <a href="https://github.com/pdonadeo/e-uscito-joypad/issues">qui</a>.
+    <p class="nota">
+      Nota per gli autori del podcast: il sito è pubblicato gratuitamente e con licenza open
+      su <a href="https://github.com/pdonadeo/e-uscito-joypad">GitHub</a> e potete fare tutte
+      le richieste che volete <a href="https://github.com/pdonadeo/e-uscito-joypad/issues">qui</a>.
     </p>
   </>);
 }
