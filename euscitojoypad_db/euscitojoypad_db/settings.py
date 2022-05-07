0f9d208682e4a1d03232ce46c91bdc3880fb3540
Paolo Donadeo <paolo@donadeo.net>
2022-05-07 15:10:27 +0200

    Prima bozza di backoffice per data entry

---
+++ euscitojoypad_db/euscitojoypad_db/settings.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "CHANGE THIS IN PRODUCTION"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "euscitojoypad_db.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "backoffice" / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "euscitojoypad_db.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "euscitojoypad_db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "it-it"
+
+TIME_ZONE = "Europe/Rome"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_ROOT = BASE_DIR / "STATIC"
+STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / "MEDIA"
+MEDIA_URL = "files/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+################################################################################
+#####                      CONFIGURAZIONE SESSIONI                         #####
+################################################################################
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+################################################################################
---
+++ euscitojoypad_db/euscitojoypad_db/settings_docker.py
@@ -0,0 +1,67 @@
+import os
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
+
+from .settings import *
+
+
+def bool_of_string(b_s):
+    if b_s == "True":
+        return True
+    elif b_s == "False":
+        return False
+    else:
+        raise ValueError("bool_of_string: %s is not a valid bool representation")
+
+
+DEBUG = bool_of_string(os.environ.get("DEBUG", "False"))
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", None).split(",")
+SECRET_KEY = os.environ.get("SECRET_KEY", None)
+
+################################################################################
+#####                      CONFIGURAZIONE SESSIONI                         #####
+################################################################################
+SESSION_COOKIE_AGE = int(os.environ.get("SESSION_COOKIE_AGE", 1209600))
+SESSION_COOKIE_NAME = os.environ.get("SESSION_COOKIE_NAME", "sessionid")
+################################################################################
+
+################################################################################
+#                             L  O  G  G  I  N  G                              #
+################################################################################
+LOGGING_CONFIG = None
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                # exact format is not important, this is the minimum information
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            },
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "console",
+            },
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+        },
+        "loggers": {
+            # root logger
+            "": {
+                "level": "INFO",
+                "handlers": [
+                    "console",
+                ],
+            },
+            "django.server": {
+                "handlers": ["console"],
+                "level": "INFO",
+                "propagate": False,
+            },
+        },
+    }
+)
+################################################################################
