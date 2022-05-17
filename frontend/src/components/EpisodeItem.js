cb39cca5077ea266bb73329cca03c2420cf02ba2
marco <marco.giuliotti@gmail.com>
2022-05-18 00:43:16 +0200

    stile: listing e card desktop

--- frontend/src/components/EpisodeItem.js
+++ frontend/src/components/EpisodeItem.js
@@ -1,18 +1,23 @@
 import ListTransition from "../CustomHooks/ListTransition";
 import classes from "./EpisodeItem.module.css";
 
+import { ReactComponent as PlusIcon } from "../icons/ICN_Plus.svg";
+
 const EpisodeItem = (props) => {
   return (
     <ListTransition className={classes.card}>
-      <div>
-        <img className={classes.cover} src={props.cover} alt={`cover dell'episodio ${props.numero}`} />
-      </div>
+      <img className={classes.cover} src={props.cover} alt={`cover dell'episodio ${props.numero}`} />
       <div className={classes.textBox}>
-        <p>{props.numero}</p>
-        <p>{props.titolo}</p>
-        <p>{(props.durata / 60).toFixed(0)} min</p>
-        <p>Data di uscita:{props.uscita}</p>
-        <a href={props.url}>Ascolta Qua</a>
+        <p className={classes.number}>{props.numero}.</p>
+        <p className={classes.title}>{props.titolo}</p>
+        <div className={classes.additionalInfo}>
+          <p className={classes.releaseDate}>{props.uscita.replaceAll("-", ".")} </p>
+          <p>&middot;</p>
+          <p className={classes.duration}>{(props.durata / 60).toFixed(0)} min.</p>
+        </div>
+        <div className={classes.actions}>
+          <PlusIcon />
+        </div>
       </div>
     </ListTransition>
   );
