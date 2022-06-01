557f75d6276133f4cd610ccc4c2c272862f08853
marco <marco.giuliotti@gmail.com>
2022-06-01 17:59:16 +0200

    aggiunta condizione per il rendering del contenuto puntata

--- frontend/src/components/EpisodeItem.js
+++ frontend/src/components/EpisodeItem.js
@@ -16,7 +16,6 @@ const EpisodeItem = (props) => {
     props.onActive(props.index, active);
     const yBox = cardRef.current.offsetTop;
     window.scrollTo({ top: yBox, behavior: "smooth" });
-    console.log(yBox);
   };
 
   return (
