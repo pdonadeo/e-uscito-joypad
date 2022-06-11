513ec3be47308bc6f11d63a57a0a6c389f6f516c
marco <marco.giuliotti@gmail.com>
2022-06-11 17:18:16 +0200

    render condizionale dell'immagine header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,30 @@ import React, { useState, useEffect, Fragment } from "react";
 import EpisodeSection from "./components/EpisodeSection";
 import Message from "./components/Hero/Message";
 import HeroLinks from "./components/Hero/HeroLinks";
+import Logo from "./components/Hero/Logo";
 
 const App = () => {
-  const [datiUltimaPuntata, setDatiUltimaPuntata] = useState({});
+    const [datiUltimaPuntata, setDatiUltimaPuntata] = useState({});
 
-  useEffect(() => {
-    fetch("/api/ultima-puntata")
-      .then((response) => response.json())
-      .then((data) => {
-        setDatiUltimaPuntata(data);
-      });
-  }, []);
+    useEffect(() => {
+        fetch("/api/ultima-puntata")
+            .then((response) => response.json())
+            .then((data) => {
+                setDatiUltimaPuntata(data);
+            });
+    }, []);
 
-  return (
-    <Fragment>
-      <div>
-        <Message dati={datiUltimaPuntata} />
-        <HeroLinks />
-        {/* {datiUltimaPuntata.fretta ? <p>Corri! Se aspetti ancora un po' esce il prossimo episodio!</p> : ""} */}
-      </div>
-      <EpisodeSection />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <header style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                <Logo />
+                <Message dati={datiUltimaPuntata} />
+                <HeroLinks />
+                {/* {datiUltimaPuntata.fretta ? <p>Corri! Se aspetti ancora un po' esce il prossimo episodio!</p> : ""} */}
+            </header>
+            <EpisodeSection />
+        </Fragment>
+    );
 };
 
 export default App;
