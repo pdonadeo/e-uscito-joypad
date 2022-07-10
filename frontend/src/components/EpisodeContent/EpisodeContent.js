8b5cdbfa3abb2702265d7bf6cec84aeca93c3222
Marco <marco@MacBook-Pro-di-Marco-Bitbull.local>
2022-07-10 20:33:47 +0200

    aggiunta descrizione agli episodi

--- frontend/src/components/EpisodeContent/EpisodeContent.js
+++ frontend/src/components/EpisodeContent/EpisodeContent.js
@@ -4,82 +4,74 @@ import classes from "./EpisodeContent.module.css";
 import GameBox from "./GameBox";
 
 const EpisodeContent = (props) => {
-    const [section, setSection] = useState(props.section);
-    const { recensioni, consigli, chiacchiere } = props;
+   const [section, setSection] = useState(props.section);
+   const { recensioni, consigli, chiacchiere, descrizione } = props;
 
-    const createContent = (section) => {
-        return (
-            <div>
-                {section.map((element, index) => (
-                    <GameBox
-                        key={index}
-                        titolo={element.titolo}
-                        speaker={element.speaker}
-                        istante={element.istante}
-                        descrizione={element.descrizione_html}
-                        cover={element.cover}
-                    />
-                ))}
-            </div>
-        );
-    };
+   const createContent = (section) => {
+      return (
+         <div>
+            {section.map((element, index) => (
+               <GameBox
+                  key={index}
+                  titolo={element.titolo}
+                  speaker={element.speaker}
+                  istante={element.istante}
+                  descrizione={element.descrizione_html}
+                  cover={element.cover}
+               />
+            ))}
+         </div>
+      );
+   };
 
-    let sectionContent;
+   let sectionContent;
 
-    if (section === "recensioni") sectionContent = createContent(props.recensioni);
-    if (section === "consigli") sectionContent = createContent(props.consigli);
-    if (section === "chiacchiere") sectionContent = createContent(props.chiacchiere);
+   if (section === "recensioni") sectionContent = createContent(props.recensioni);
+   if (section === "consigli") sectionContent = createContent(props.consigli);
+   if (section === "chiacchiere") sectionContent = createContent(props.chiacchiere);
 
-    if (section === "descrizione")
-        sectionContent = (
-            <div>
-                <p className={classes.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum
-                </p>
-            </div>
-        );
+   if (section === "descrizione")
+      sectionContent = (
+         <div>
+            <p className={classes.description}>{descrizione}</p>
+         </div>
+      );
 
-    const sectionHandler = (section) => {
-        setSection(section);
-    };
+   const sectionHandler = (section) => {
+      setSection(section);
+   };
 
-    return (
-        <div className={classes.containerAnimation}>
-            <ul className={classes.controls}>
-                {recensioni.length !== 0 && (
-                    <li onClick={sectionHandler.bind(this, "recensioni")}>
-                        <p className={`${classes.control} ${section === "recensioni" && classes.tagActive}`}>
-                            RECENSIONI <span>{recensioni.length}</span>
-                        </p>
-                    </li>
-                )}
-                {consigli.length !== 0 && (
-                    <li onClick={sectionHandler.bind(this, "consigli")}>
-                        <p className={`${classes.control} ${section === "consigli" && classes.tagActive}`}>
-                            CONSIGLI <span>{consigli.length}</span>
-                        </p>
-                    </li>
-                )}
-                {chiacchiere.length !== 0 && (
-                    <li onClick={sectionHandler.bind(this, "chiacchiere")}>
-                        <p className={`${classes.control} ${section === "chiacchiere" && classes.tagActive}`}>
-                            CHIACCHIERE <span>{chiacchiere.length}</span>
-                        </p>
-                    </li>
-                )}
-                <li onClick={sectionHandler.bind(this, "descrizione")}>
-                    <p className={`${classes.control} ${section === "descrizione" && classes.tagActive}`}>
-                        DESCRIZIONE
-                    </p>
-                </li>
-            </ul>
-            <div className={classes.contentContainer}>{sectionContent}</div>
-        </div>
-    );
+   return (
+      <div className={classes.containerAnimation}>
+         <ul className={classes.controls}>
+            {recensioni.length !== 0 && (
+               <li onClick={sectionHandler.bind(this, "recensioni")}>
+                  <p className={`${classes.control} ${section === "recensioni" && classes.tagActive}`}>
+                     RECENSIONI <span>{recensioni.length}</span>
+                  </p>
+               </li>
+            )}
+            {consigli.length !== 0 && (
+               <li onClick={sectionHandler.bind(this, "consigli")}>
+                  <p className={`${classes.control} ${section === "consigli" && classes.tagActive}`}>
+                     CONSIGLI <span>{consigli.length}</span>
+                  </p>
+               </li>
+            )}
+            {chiacchiere.length !== 0 && (
+               <li onClick={sectionHandler.bind(this, "chiacchiere")}>
+                  <p className={`${classes.control} ${section === "chiacchiere" && classes.tagActive}`}>
+                     CHIACCHIERE <span>{chiacchiere.length}</span>
+                  </p>
+               </li>
+            )}
+            <li onClick={sectionHandler.bind(this, "descrizione")}>
+               <p className={`${classes.control} ${section === "descrizione" && classes.tagActive}`}>DESCRIZIONE</p>
+            </li>
+         </ul>
+         <div className={classes.contentContainer}>{sectionContent}</div>
+      </div>
+   );
 };
 
 export default EpisodeContent;
--- frontend/src/components/EpisodeItem.js
+++ frontend/src/components/EpisodeItem.js
@@ -10,99 +10,98 @@ import EpisodeNumber from "./UI/EpisodeNumber";
 import PlayButton from "./UI/PlayButton";
 
 const EpisodeItem = (props) => {
-    const [section, setSection] = useState("recensioni");
-    const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
+   const [section, setSection] = useState("recensioni");
+   const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
 
-    const y = 605 + props.index * 138;
+   const y = 605 + props.index * 138;
 
-    // const yBox = cardRef.current?.offsetTop;
-    // yBox ? console.log(props.numero, yBox) : console.log(null);
+   // const yBox = cardRef.current?.offsetTop;
+   // yBox ? console.log(props.numero, yBox) : console.log(null);
 
-    // const [active, setActive] = useState(false);
+   // const [active, setActive] = useState(false);
 
-    const activeHandler = (event) => {
-        // window.scrollTo({ top: y, behavior: "smooth" });
-        // setActive(!active);
-        // setTimeout(() => {
-        // event.preventDefault();
-        props.onActive(props.index, props.active, props.id, y);
-        // }, 500);
-    };
+   const activeHandler = (event) => {
+      // window.scrollTo({ top: y, behavior: "smooth" });
+      // setActive(!active);
+      // setTimeout(() => {
+      // event.preventDefault();
+      props.onActive(props.index, props.active, props.id, y);
+      // }, 500);
+   };
 
-    const checkEvenIndex = (index) => {
-        return index % 2 === 0;
-    };
+   const checkEvenIndex = (index) => {
+      return index % 2 === 0;
+   };
 
-    // let activation = props.cardState;
-    // if (props.active) {
-    //   activation = true;
-    // } else {
-    //   activation = props.cardState;
-    // }
+   // let activation = props.cardState;
+   // if (props.active) {
+   //   activation = true;
+   // } else {
+   //   activation = props.cardState;
+   // }
 
-    // useEffect(() => {
-    //   console.log("rendering");
-    //   console.log(activation);
-    // }, [activation]);
+   // useEffect(() => {
+   //   console.log("rendering");
+   //   console.log(activation);
+   // }, [activation]);
 
-    const recensioni = props.giochi.filter((gioco) => gioco.tipologia === "Recensione");
-    const chiacchiere = props.giochi.filter((gioco) => gioco.tipologia === "Chiacchiera libera");
-    const consigli = props.giochi.filter((gioco) => gioco.tipologia === "Consiglio");
+   const recensioni = props.giochi.filter((gioco) => gioco.tipologia === "Recensione");
+   const chiacchiere = props.giochi.filter((gioco) => gioco.tipologia === "Chiacchiera libera");
+   const consigli = props.giochi.filter((gioco) => gioco.tipologia === "Consiglio");
 
-    useEffect(() => {
-        const groupsArr = [recensioni, consigli, chiacchiere];
-        const firstActive = groupsArr.findIndex((group) => group.length !== 0);
-        if (firstActive === -1) setSection("descrizione");
-        if (firstActive === 0) setSection("recensioni");
-        if (firstActive === 1) setSection("consigli");
-        if (firstActive === 2) setSection("chiacchiere");
-    }, []);
+   useEffect(() => {
+      const groupsArr = [recensioni, consigli, chiacchiere];
+      const firstActive = groupsArr.findIndex((group) => group.length !== 0);
+      if (firstActive === -1) setSection("descrizione");
+      if (firstActive === 0) setSection("recensioni");
+      if (firstActive === 1) setSection("consigli");
+      if (firstActive === 2) setSection("chiacchiere");
+   }, []);
 
-    return (
-        <li className={classes.cardBox}>
-            <ListTransition className={`${classes.card} ${props.active && classes.active}`} onClick={activeHandler}>
-                <img className={classes.cover} src={props.cover} alt={`cover dell'episodio ${props.numero}`} />
-                <div className={classes.textBox}>
-                    <div className={classes.firstBox}>
-                        <EpisodeNumber numero={props.numero} />
-                        <div className={classes.playButton}>
-                            <PlayButton url={props.url} />
-                        </div>
-                    </div>
-                    <p className={classes.title}>{props.titolo}</p>
-                    <div className={classes.additionalInfo}>
-                        <p className={classes.releaseDate}>{props.uscita.replaceAll("-", ".")} </p>
-                        <p>&middot;</p>
-                        <p className={classes.duration}>{(props.durata / 60).toFixed(0)} min.</p>
-                    </div>
-                    <div className={classes.actions}>
-                        {/* <a href={props.url}> */}
-                        <p>DETTAGLI</p>
-                        <PlusIcon />
-                        {/* </a> */}
-                    </div>
-                </div>
-            </ListTransition>
-            {props.active ? (
-                <div
-                    className={classes.adding}
-                    /* style={
-                        !isMobile ? { transform: `translateX(${checkEvenIndex(props.index) ? "50%" : "-50%"})` } : {}
-                    } */
-                >
-                    <EpisodeContent
-                        section={section}
-                        recensioni={recensioni}
-                        consigli={consigli}
-                        chiacchiere={chiacchiere}
-                        giochi={props.giochi}
-                    />
-                </div>
-            ) : (
-                ""
-            )}
-        </li>
-    );
+   return (
+      <li className={classes.cardBox}>
+         <ListTransition className={`${classes.card} ${props.active && classes.active}`} onClick={activeHandler}>
+            <img className={classes.cover} src={props.cover} alt={`cover dell'episodio ${props.numero}`} />
+            <div className={classes.textBox}>
+               <div className={classes.firstBox}>
+                  <EpisodeNumber numero={props.numero} />
+                  <div className={classes.playButton}>
+                     <PlayButton url={props.url} />
+                  </div>
+               </div>
+               <p className={classes.title}>{props.titolo}</p>
+               <div className={classes.additionalInfo}>
+                  <p className={classes.releaseDate}>{props.uscita.replaceAll("-", ".")} </p>
+                  <p>&middot;</p>
+                  <p className={classes.duration}>{(props.durata / 60).toFixed(0)} min.</p>
+               </div>
+               <div className={classes.actions}>
+                  {/* <a href={props.url}> */}
+                  <p>DETTAGLI</p>
+                  <PlusIcon />
+                  {/* </a> */}
+               </div>
+            </div>
+         </ListTransition>
+         {props.active ? (
+            <div
+               className={classes.adding}
+               style={!isMobile ? { transform: `translateX(${checkEvenIndex(props.index) ? "50%" : "-50%"})` } : {}}
+            >
+               <EpisodeContent
+                  section={section}
+                  recensioni={recensioni}
+                  consigli={consigli}
+                  chiacchiere={chiacchiere}
+                  giochi={props.giochi}
+                  descrizione={props.descrizione}
+               />
+            </div>
+         ) : (
+            ""
+         )}
+      </li>
+   );
 };
 
 export default EpisodeItem;
