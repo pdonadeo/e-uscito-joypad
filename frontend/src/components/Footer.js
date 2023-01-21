dc03a0c5fd7e4f7ea8ee4157451182e1b663b710
marco <marco.giuliotti@gmail.com>
2023-01-21 15:53:24 +0100

    Spostati i riferimenti discord e instagram da footer a hero - corrette icone

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,23 +1,21 @@
-import { ReactComponent as Instagram } from '../icons/ICN_Instagram.svg';
-import { ReactComponent as Discord } from '../icons/ICN_Discord.svg';
+
 import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>
-        Copyright © 2022 Paolo Donadeo.</p>
-      <p>
-        Rilasciato sotto licenza MIT, vedi
-        <a href="https://github.com/pdonadeo/e-uscito-joypad/blob/main/LICENSE">LICENSE.md</a>
+      <p className={classes.note}>
+        Nota per gli autori del podcast: il sito è pubblicato gratuitamente e
+        con licenza open su
+        <a href="https://github.com/pdonadeo/e-uscito-joypad" rel="noreferrer" target="_blank"> GitHub</a> e
+        potete fare tutte le richieste che volete
+        <a href="https://github.com/pdonadeo/e-uscito-joypad/issues" rel="noreferrer" target="_blank"> qui</a>.
       </p>
       <p>
-        <a href="//www.instagram.com/corrisaltaspara/" target="_blank" rel="noreferrer">
-          <Instagram className={classes.bottomIcon} />
-        </a>
-        <a href="//discord.gg/6KYbyutq9X" target="_blank" rel="noreferrer">
-          <Discord className={classes.bottomIcon} />
-        </a>
+        Copyright © 2023 Paolo Donadeo.</p>
+      <p>
+        Rilasciato sotto licenza MIT, vedi
+        <a href="https://github.com/pdonadeo/e-uscito-joypad/blob/main/LICENSE" rel="noreferrer" target="_blank">LICENSE.md</a>
       </p>
     </footer>
   );
--- frontend/src/components/Hero/HeroLinks.js
+++ frontend/src/components/Hero/HeroLinks.js
@@ -4,38 +4,44 @@ import { ReactComponent as IlPost } from "../../icons/ICN_IlPost.svg";
 import { ReactComponent as Spotify } from "../../icons/ICN_Spotify.svg";
 import { ReactComponent as Apple } from "../../icons/ICN_Apple.svg";
 import { ReactComponent as Google } from "../../icons/ICN_Google.svg";
+import { ReactComponent as Instagram } from "../../icons/ICN_Instagram.svg";
+import { ReactComponent as Discord } from "../../icons/ICN_Discord.svg";
 
 const HeroLinks = () => {
   return (
     <div className={classes.container}>
       <p className={classes.listenMessage}>
         Ascolta la puntata{" "}
-        <a href="https://www.ilpost.it/podcasts/joypad/">
+        <a href="https://www.ilpost.it/podcasts/joypad/" rel="noreferrer" target="_blank">
           sulla pagina del Post
         </a>
         ,<br /> oppure
       </p>
       <div className={classes.linkBoxes}>
-        <a href="https://app.ilpost.it/">
+        <a href="https://app.ilpost.it/" rel="noreferrer" target="_blank">
           <IlPost />
         </a>
-        <a href="https://open.spotify.com/show/2JXX6er1IfNvbyPbYPI9tJ">
+        <a href="https://open.spotify.com/show/2JXX6er1IfNvbyPbYPI9tJ" rel="noreferrer" target="_blank">
           <Spotify />
         </a>
-        <a href="https://podcasts.apple.com/us/podcast/joypad/id1491137742?uo=4">
+        <a href="https://podcasts.apple.com/us/podcast/joypad/id1491137742?uo=4" rel="noreferrer" target="_blank">
           <Apple />
         </a>
-        <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1MTQyOS9lcGlzb2Rlcy9mZWVk">
+        <a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvNDE1MTQyOS9lcGlzb2Rlcy9mZWVk" rel="noreferrer" target="_blank">
           <Google />
         </a>
       </div>
-      <p className={classes.note}>
-        Nota per gli autori del podcast: il sito è pubblicato gratuitamente e
-        con licenza open su
-        <a href="https://github.com/pdonadeo/e-uscito-joypad"> GitHub</a> e
-        potete fare tutte le richieste che volete
-        <a href="https://github.com/pdonadeo/e-uscito-joypad/issues"> qui</a>.
-      </p>
+      <div className={classes.followUs}>
+        <p>
+          Seguici su:
+        </p>
+          <a href="//discord.gg/6KYbyutq9X" rel="noreferrer" target="_blank">
+            <Discord/>
+          </a>
+          <a href="//www.instagram.com/corrisaltaspara/" rel="noreferrer" target="_blank">
+           <Instagram/>
+          </a>
+      </div>
     </div>
   );
 };
