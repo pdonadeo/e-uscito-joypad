1bec34303f2a87813b0f5f822b9625e98db8bb94
marco <marco.giuliotti@gmail.com>
2023-01-26 19:06:51 +0100

    setup base di evidenziazione e stile per i risultati di ricerca

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -14,6 +14,8 @@ const SearchBar = () => {
   const [showModal, setShowModal] = useState(false);
   const [sorting, setSorting] = useState('ascending');
   const [resultList, setResultList] = useState([]);
+  const [focus, setFocus] = useState(0);
+  const [searchWords, setSearchWords] = useState('');
 
   const toggleShowModal = () => {
     setShowModal((oldState) => !oldState);
@@ -28,12 +30,46 @@ const SearchBar = () => {
   const getSearchResults = async (searchWords) => {
     if(searchWords.length  === 0) {setResultList([]); return;}
 
-    const response = await fetch(`http://localhost:5000/api/search-game-title/${searchWords}`);
+    setSearchWords(searchWords);
+    const response = await fetch(`/api/search-game-title/${searchWords}`);
     const data = await response.json();
-    console.log(data.result);
-
+    
     const listNames = data.result.map(({titolo, id}) => ({titolo, id}));
     setResultList(listNames);
+  }
+
+  const boldName = (name, search) =>{
+    
+    const isMatch = name.toLowerCase().match(search.toLowerCase());
+    if(isMatch) {
+    const index = isMatch.index;
+
+    if (index !== 0) {
+        return <>
+          {name.substr(0, index)}<strong>{name.substr(index, search.length)}</strong>{name.substr((index+search.length), name.length)}
+        </>
+      }
+    }
+    if(name.substr(0,search.length).toLowerCase() ===search.toLowerCase() ){ 
+      return <>
+    <strong>{name.substr(0, search.length)}</strong>{name.substr(search.length)};
+    </>
+    }
+    return name;
+  }
+
+  const onKeyDown = (event) =>{
+    if(event.key === 'ArrowDown'){
+       setFocus(()=> focus + 1);
+    }
+    if(event.key === 'ArrowUp'){
+       setFocus(()=> focus - 1);
+    }
+    if(event.key === 'Enter') {
+      searchCtx.setSearchInput(resultList[focus].titolo);
+      setResultList([]);
+      setFocus(0);
+    }
   }
 
   const checkIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`${classes.checkIcon}`}>
@@ -48,23 +84,25 @@ const SearchBar = () => {
         type="text"
         placeholder={"| Cerca un gioco…"}
         className={classes.input}
+        id="search-bar"
         minLength={4}
         debounceTimeout={300}
         onChange={(ev) => { 
           // searchCtx.setSearchInput(ev.target.value);
           getSearchResults(ev.target.value) 
-          console.log(ev.target.value);
         }}
         value={searchCtx.searchInput}
+        onKeyDown={onKeyDown}
       />
       {resultList.length > 0 && 
       <div className={classes.searchSuggestions}>
-        {resultList.map(result=> 
+        {resultList.map((result, i)=> 
         <div 
-          className={classes.suggestion} 
+          // style={ i === focus ? {backgroundColor: '#f7f7f8'} : {}}
+          className={`${classes.suggestion} ${i === focus? classes.keyActive : ''}`} 
           key={result.id} 
           onClick={()=> {searchCtx.setSearchInput(result.titolo); setResultList([])}}>
-            <p >{result.titolo}</p>
+            <p >{boldName(result.titolo, searchWords)}</p>
           </div>
         )}
         </div>}
