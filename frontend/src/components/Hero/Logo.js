c2c1e10d017db53e08e0af460ab7addbb1ceec98
marco <marco.giuliotti@gmail.com>
2024-12-22 14:19:29 +0100

    Fix logo dimensions in desktop and mobile

--- frontend/src/components/Hero/Logo.js
+++ frontend/src/components/Hero/Logo.js
@@ -1,23 +1,18 @@
-import { useMediaQuery } from "react-responsive";
-
 import classes from "./Logo.module.css";
 
 const Logo = () => {
-  
-  const isMobile = useMediaQuery({ query: "(max-width: 920px)" });
-
-  return (
-    <div className={classes.logoContainer}>
-      <a tabIndex={0} aria-label="Benvenuti su Joypad, Corri, salta, spara" href="/">
-        <img
-          src={`${process.env.PUBLIC_URL}${isMobile ? "/joypad-img_mobile.jpg" : "/joypad-img.jpg"}`}
-          alt="joypad logo"
-          tabIndex={-1}
-          className={classes.logoImage}
-        />
-      </a>
-    </div>
-  );
+    return (
+        <div className={classes.logoContainer}>
+            <a tabIndex={0} aria-label="Benvenuti su Joypad, Corri, salta, spara" href="/">
+                <img
+                    src={`${process.env.PUBLIC_URL}${"/joypad-img.jpg"}`}
+                    alt="joypad logo"
+                    tabIndex={-1}
+                    className={classes.logoImage}
+                />
+            </a>
+        </div>
+    );
 };
 
 export default Logo;
