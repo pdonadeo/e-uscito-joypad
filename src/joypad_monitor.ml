c08898eddf0c7b6db7921a5d0c6ffe0b35f5737d
Paolo Donadeo <paolo@donadeo.net>
2025-10-23 22:40:54 +0200

    refactor: alza la soglia della pazienza prima di lamentarsi
    Estrae le soglie di giorni per considerare "uscito" un episodio e per
    iniziare a lamentarsi in costanti dedicate (`soglia_uscito_giorni` e
    `soglia_lamentele_giorni`). Ora, prima di protestare, bisogna aspettare
    un po' di più... la pazienza è una virtù, anche per chi aspetta Joypad!

--- src/joypad_monitor.ml
+++ src/joypad_monitor.ml
@@ -24,6 +24,8 @@ type dati_ultima_puntata = {
 
 let log = Dream.sub_log "joypad.monitor"
 let last_episode_data : data option ref = ref None
+let soglia_uscito_giorni = 24
+let soglia_lamentele_giorni = 48
 
 let body () =
   let%lwt _, body = Client.get (Uri.of_string Settings.joypad_page) in
@@ -88,15 +90,17 @@ let elabora_risposta () =
     let oggi = Timedesc.date adesso in
     let mese_corrente = Timedesc.Date.month oggi in
     let giorni_passati = Timedesc.Date.diff_days oggi last_episode_data.date in
-    let uscito = if giorni_passati <= 14 then true else false in
+    let uscito = if giorni_passati <= soglia_uscito_giorni then true else false in
     let giorni_fa = Utils.distanza giorni_passati in
     let data_italiano = Utils.string_of_date last_episode_data.date in
     let msg_risposta_no =
       match giorni_passati with
       | _n when (not uscito) && (mese_corrente = 8 || mese_corrente = 9) ->
         Some "È finita la stagione di Joypad. Aspettiamo la prossima a Settembre."
-      | n when (not uscito) && n <= 20 -> Some "Però adesso non torturare Zampa, aspetta ancora qualche giorno…"
-      | n when (not uscito) && n > 20 -> Some "Mi duole ammetterlo ma è giunto il momento di protestare!"
+      | n when (not uscito) && n <= soglia_lamentele_giorni ->
+        Some "Però adesso non torturare Zampa, aspetta ancora qualche giorno…"
+      | n when (not uscito) && n > soglia_lamentele_giorni ->
+        Some "Mi duole ammetterlo ma è giunto il momento di protestare!"
       | _ -> None
     in
     (uscito, giorni_fa, data_italiano, last_episode_data.ep_num, last_episode_data.title, msg_risposta_no)
